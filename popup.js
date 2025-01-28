d87f5b697d1404d418f502b0415f21b556551158
claire-w6 <clairewang1616@gmail.com>
2025-01-27 19:17:38 -0600

    add: add all tabs function

--- popup.js
+++ popup.js
@@ -8,6 +8,8 @@ function updateTabList() {
         const link = document.createElement('a');
         link.href = tab.url;
         link.textContent = tab.title;
+        link.onclick = () => switchToTab(tab.tabId); 
+
         listItem.appendChild(link);
         tabList.appendChild(listItem);
       });
@@ -15,15 +17,34 @@ function updateTabList() {
 }
   
 document.getElementById('saveTab').addEventListener('click', () => {
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const tab = tabs[0]; 
-    chrome.storage.local.get(['savedTabs'], (result) => { 
-    let savedTabs = result.savedTabs || []; 
-    savedTabs.push(tab); 
-    chrome.storage.local.set({ savedTabs: savedTabs }); 
-    updateTabList(); 
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const tab = tabs[0]; 
+        chrome.storage.local.get(['savedTabs'], (result) => { 
+        let savedTabs = result.savedTabs || [];
+        const tabData = {
+            url: tab.url,
+            title: tab.title,
+            tabId: tab.id
+        }
+        savedTabs.push(tabData); 
+        chrome.storage.local.set({ savedTabs: savedTabs }); 
+        updateTabList(); 
+        });
     });
-});
+})
+
+document.getElementById('addAll').addEventListener('click', () => {
+    chrome.tabs.query({}, (tabs) => {
+
+        const allTabData = tabs.map(tab => ({ 
+        url: tab.url, 
+        title: tab.title, 
+        id: tab.id 
+        }));
+
+        chrome.storage.local.set({ savedTabs: allTabData });
+        updateTabList();
+    });
 })
 
 document.getElementById('clearList').addEventListener('click', () => {
@@ -31,5 +52,9 @@ document.getElementById('clearList').addEventListener('click', () => {
         updateTabList(); 
     });
 });
+
+function switchToTab(tabId) {
+    chrome.tabs.update(tabId, { active: true });
+}
 
 updateTabList();
